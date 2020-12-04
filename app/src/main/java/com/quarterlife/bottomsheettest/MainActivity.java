1703aac131e52986cdce582f2fc8d47e162802ab
LulaKao <windandsprings@gmail.com>
2020-12-04 18:31:17 +0800

    create bottom sheet dialog -> OK

---
+++ app/src/main/java/com/quarterlife/bottomsheettest/MainActivity.java
@@ -0,0 +1,42 @@
+package com.quarterlife.bottomsheettest;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btn_dialog, btn_fragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // initView
+        btn_dialog = findViewById(R.id.btn_dialog);
+        btn_fragment= findViewById(R.id.btn_dialog_fragment);
+
+        // 設置 btn_dialog 的點擊事件
+        btn_dialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // 在 MainActivity 裡開啟 BottomSheetDialog 載入一個 Layout（bottom_sheet.xml）
+                BottomSheetDialog dialog = new BottomSheetDialog(MainActivity.this);
+                dialog.setContentView(R.layout.bottom_sheet);
+//                getLayoutInflater().inflate(R.layout.bottom_sheet, null);
+                dialog.show();
+            }
+        });
+
+        // 設置 btn_fragment 的點擊事件
+        btn_fragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+}
